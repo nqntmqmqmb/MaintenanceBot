5039f97d3d26b4ae6254b3c6b5bb5f1b4099faa2
NQNT <54726985+nqntmqmqmb@users.noreply.github.com>
2020-08-07 13:55:45 +0200

    Update bot.py
    Add custom status

--- bot.py
+++ bot.py
@@ -12,6 +12,7 @@ client = discord.Client()
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
+    await client.change_presence(activity=discord.Game(name="Need help ? Use .help"))
 
 @client.event
 async def on_message(message):
